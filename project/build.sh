9bd9998b749c5cda9dd95e7ae65d7528b2161a3a
= <=>
2024-12-08 12:36:54 +0530

    Modify build script and vercel.json.

--- project/build.sh
+++ project/build.sh
@@ -3,12 +3,12 @@ set -e
 echo "Vercel Build Begins!!!!!!!!"
 echo "Current user: $(whoami)"
 echo "Installing requirements......"
+pip install setuptools
 pip install -r requirements.txt
 echo "Requirements installed successfully!"
-ls -la
-chmod -R u+w project/
-cd project/
-python3 manage.py collectstatic
+
+
 python3 manage.py wait_for_db 
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py collectstatic
