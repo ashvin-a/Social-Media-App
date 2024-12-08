202e73b1e798e0f37f0852296981b4d258da77a8
= <=>
2024-12-08 12:10:55 +0530

    Test bash script.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "Current user: $(whoami)"
 echo "Installing requirements......"
 pip install -r requirements.txt
 echo "Requirements installed successfully!"
-
+ls -la
 chmod -R u+w project/
 
 python3 project/manage.py collectstatic
