f82ede7037e21e746388155672a1d1451d0c6423
= <=>
2024-12-08 12:27:48 +0530

    Test deployment in vercel.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# /bin/bash
 set -e  
 echo "Vercel Build Begins!!!!!!!!"
 echo "Current user: $(whoami)"
@@ -7,5 +7,8 @@ pip install -r requirements.txt
 echo "Requirements installed successfully!"
 ls -la
 chmod -R u+w project/
-
-python3 project/manage.py collectstatic
+cd project/
+python3 manage.py collectstatic
+python3 manage.py wait_for_db 
+python3 manage.py makemigrations
+python3 manage.py migrate
