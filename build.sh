70c72135da87e687e22bc5ce994625ff068f9323
= <=>
2024-12-08 12:03:41 +0530

    Modify bash script.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+echo "Vercel Build Begins!!!!!!!!"
+echo "Installing requirements......"
 pip install -r requirements.txt
+echo "Requirements installed successfully!"
 
 python3.9 project/manage.py collectstatic
