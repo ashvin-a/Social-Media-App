145dd04017550279048132f276ad870de33fd71c
= <=>
2024-12-08 12:08:18 +0530

    Modify build file to rectify issue with permissions.

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e  
 echo "Vercel Build Begins!!!!!!!!"
+echo "Current user: $(whoami)"
 echo "Installing requirements......"
 pip install -r requirements.txt
 echo "Requirements installed successfully!"
 
-python3.9 project/manage.py collectstatic
+chmod -R u+w project/
+
+python3 project/manage.py collectstatic
